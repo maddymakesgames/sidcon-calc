a8fdd3a824a5d7098d05db8862ad394fbf4a6aa5
Madeline Schreiber <madelinemakesgames@gmail.com>
2026-04-02 11:45:01 -0400

    Fix settings styling

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -16,19 +16,21 @@ function GameSettings({setFaction, setTurn, setPlayerCount}: Inputs) {
     }
 
     return <>
-        <form className="row">
-            <div className="col input-group">
-                <FactionSelect setFaction={setFaction} />
-            </div>
-            <div className="col input-group">
-                <label htmlFor="turn-input" className="input-group-text">Current Turn:</label>
-                <input className="form-control" id="turn-input" type="number" value={1} min={1} max={6} onInput={onTurn} />
-            </div>
-            <div className="col input-group">
-                <label htmlFor="player-input" className="input-group-text">Player Count:</label>
-                <input className="form-control" id="player-input" type="number" value={3} min={3} max={9} onInput={onPlayerCount} />
-            </div>
-        </form>
+        <div className="container mt-1 mb-1">
+            <form className="row">
+                <div className="col input-group">
+                    <FactionSelect setFaction={setFaction} />
+                </div>
+                <div className="col input-group">
+                    <label htmlFor="turn-input" className="input-group-text">Current Turn:</label>
+                    <input className="form-control" id="turn-input" type="number" value={1} min={1} max={6} onInput={onTurn} />
+                </div>
+                <div className="col input-group">
+                    <label htmlFor="player-input" className="input-group-text">Player Count:</label>
+                    <input className="form-control" id="player-input" type="number" value={3} min={3} max={9} onInput={onPlayerCount} />
+                </div>
+            </form>
+        </div>
     </>
 }
 
